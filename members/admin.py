4f0bf3d1456e9b58503fe8eeeca90eb190b06637
Amos Omariba <amomariba@gmail.com>
2025-05-29 18:35:41 +0300

    add Member on admin

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Member
 
-# Register your models here.
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "phone_number", "joined_date")
+    prepopulated_fields = {"slug": ("firstname", "lastname")}
+
+
+admin.site.register(Member, MemberAdmin)
--- members/models.py
+++ members/models.py
@@ -7,6 +7,7 @@ class Member(models.Model):
     age = models.IntegerField(null=True, blank=True)
     phone_number = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
+    slug = models.SlugField(default="", null=False)
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
