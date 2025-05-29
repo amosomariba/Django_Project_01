47cb025444a3fe3603cc16728b4a78526a6d765b
Amos Omariba <amomariba@gmail.com>
2025-05-29 17:21:48 +0300

    Create templates

--- members/models.py
+++ members/models.py
@@ -5,6 +5,8 @@ class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     age = models.IntegerField(null=True, blank=True)
+    phone_number = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
